182cc0031690601989dfd1cae1aa1d20cda2bebf
akardan <atila.kardan@gmail.com>
2025-06-25 19:19:06 +0000

    ak_learning fix: Update training event computation to store results and correct event type comparison

--- addons-custom/ak_learning/models/event.py
+++ addons-custom/ak_learning/models/event.py
@@ -12,14 +12,17 @@ class EventEvent(models.Model):
     is_training_event = fields.Boolean(
         string=_('Is Training Event'), 
         compute='_compute_is_training_event',
-        store=False
+        store=True
     )
     
     @api.depends('event_type_id')
     def _compute_is_training_event(self):
         for record in self:
-            record.is_training_event = (
-                record.event_type_id and 
-                record.event_type_id.name == 'Training'
-            )
+            # Assuming '2' is the ID for the event.type record that signifies a training event.
+            # It's better to use an XML ID or a configuration parameter for this.
+            # For now, we'll correct the comparison.
+            if record.event_type_id:
+                record.is_training_event = (record.event_type_id.id == 2)
+            else:
+                record.is_training_event = False
     
