a87118ebc8654b1c93972c15d36c31af8b39bcf2
akardan <atila.kardan@gmail.com>
2025-11-24 02:10:10 +0000

    FIX: Correct assignment of inverse_company_rate in exchange rate update

--- addons-custom/ak_currency_rate_tcmb/models/tcmb_rate_update.py
+++ addons-custom/ak_currency_rate_tcmb/models/tcmb_rate_update.py
@@ -66,7 +66,7 @@ class ResCurrencyRate(models.Model):
                         # Update the existing rate
                         existing_rate.write({
                             # "rate": rate_value, # Corrected: Odoo rate is 1/ForexBuying
-                            "inverse_rate": inverse_rate_value, # Corrected: Inverse rate is ForexBuying
+                            "inverse_company_rate": inverse_rate_value, # Corrected: Inverse rate is ForexBuying
                         })
                         _logger.info(f"Updated exchange rate for {code} to {rate_value} on {effective_date}.")
                     else:
@@ -75,7 +75,7 @@ class ResCurrencyRate(models.Model):
                             "currency_id": odoo_currency.id,
                             "name": effective_date,
                             # "rate": rate_value, # Corrected: Odoo rate is 1/ForexBuying
-                            "inverse_rate": inverse_rate_value, # Corrected: Inverse rate is ForexBuying
+                            "inverse_company_rate": inverse_rate_value, # Corrected: Inverse rate is ForexBuying
                             "company_id": self.env.company.id,
                         })
                         _logger.info(f"Created new exchange rate for {code} to {rate_value} on {effective_date}.")
